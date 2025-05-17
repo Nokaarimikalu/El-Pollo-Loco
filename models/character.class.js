09f42d5eeb2d58b484298f6b2dbd31ed78298c19
Pascal Fliednr <fliednerpascal@gmail.com>
2025-05-17 21:24:21 +0200

    V3

--- models/character.class.js
+++ models/character.class.js
@@ -4,21 +4,36 @@ class Character extends MoveableObject {
     height = 250;
     width = 250;
 
+    IMAGES_WAlKING = [
+        `own.img/character/walking/walk_frame_1.png`,
+        `own.img/character/walking/walk_frame_2.png`,
+        `own.img/character/walking/walk_frame_3.png`,
+        `own.img/character/walking/walk_frame_4.png`,
+        `own.img/character/walking/walk_frame_5.png`,
+        `own.img/character/walking/walk_frame_6.png`,
+        `own.img/character/walking/walk_frame_7.png`,
+        `own.img/character/walking/walk_frame_8.png`,
+        `own.img/character/walking/walk_frame_9.png`,
+        `own.img/character/walking/walk_frame_10.png`,
+        `own.img/character/walking/walk_frame_11.png`,
+        `own.img/character/walking/walk_frame_12.png`,
+    ];
+
     IMAGES_IDLE = [
-        `character.img/idle/idle_frame_1.png`,
-        `character.img/idle/idle_frame_2.png`,
-        `character.img/idle/idle_frame_3.png`,
-        `character.img/idle/idle_frame_4.png`,
-        `character.img/idle/idle_frame_5.png`,
-        `character.img/idle/idle_frame_6.png`,
-        `character.img/idle/idle_frame_7.png`,
-        `character.img/idle/idle_frame_8.png`,
-        `character.img/idle/idle_frame_9.png`,
+        `own.img/character/idle/idle_frame_1.png`,
+        `own.img/character/idle/idle_frame_2.png`,
+        `own.img/character/idle/idle_frame_3.png`,
+        `own.img/character/idle/idle_frame_4.png`,
+        `own.img/character/idle/idle_frame_5.png`,
+        `own.img/character/idle/idle_frame_6.png`,
+        `own.img/character/idle/idle_frame_7.png`,
+        `own.img/character/idle/idle_frame_8.png`,
+        `own.img/character/idle/idle_frame_9.png`,
     ];
     currentImage = 0;
 
     constructor() {
-        super().loadImage(`own.img/walk_frame_1.png`);
+        super().loadImage(`own.img/character/idle/idle_frame_1.png`);
         this.loadImages(this.IMAGES_IDLE);
         this.animate();
     }
